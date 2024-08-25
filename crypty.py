069ec23f9e20d51f0637fa98b2411975de12353d
thataboy <dearbart@yahoo.com>
2024-08-24 21:15:51 -0700

    fix encrypt/decrypt when password length > 32

--- crypty.py
+++ crypty.py
@@ -1,6 +1,8 @@
 import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
 import base64
 
 CRYPT_KEY_FILE = os.path.expanduser("~/.ftpsync.rand")
@@ -27,8 +29,8 @@ CRYPT = load_key()
 def encrypt(password, key=CRYPT):
     """Encrypts the given password using the provided key."""
     cipher = AES.new(key, AES.MODE_ECB)
-    padded_password = password.ljust(32)  # Pad password to 32 bytes (AES block size)
-    encrypted_password = cipher.encrypt(padded_password.encode('utf-8'))
+    padded_password = pad(password.encode('utf-8'), AES.block_size)  # Pad to the block size
+    encrypted_password = cipher.encrypt(padded_password)
     return base64.b64encode(encrypted_password).decode('utf-8')
 
 
@@ -36,5 +38,5 @@ def decrypt(encrypted_password, key=CRYPT):
     """Decrypts the given encrypted password using the provided key."""
     encrypted_password = base64.b64decode(encrypted_password)
     cipher = AES.new(key, AES.MODE_ECB)
-    decrypted_password = cipher.decrypt(encrypted_password).decode('utf-8')
-    return decrypted_password.strip()  # Remove padding
+    decrypted_password = unpad(cipher.decrypt(encrypted_password), AES.block_size).decode('utf-8')  # Remove padding
+    return decrypted_password
